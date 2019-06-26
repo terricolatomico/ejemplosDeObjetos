7de8f85293ef3e6839c3a4077c7238c8f0995a56
terricolatomico <terricolatomico@gmail.com>
2019-06-26 10:53:31 +0100

    objetos y atributos

---
+++ objetos1.py
@@ -0,0 +1,15 @@
+class Antena():
+    color = ""
+    longitud = ""
+class Pelo():
+    color = ""
+    longitud = ""
+class Ojo():
+    color = ""
+    longitud = ""
+class objeto():
+    color = ""
+    longitud = ""
+    antenas = Antena()  #propiedad compuesta por el objeto Antena()
+    ojos = Ojo()        #propiedad compuesta por el objeto Ojo()
+    pelos = Pelo()      #propiedad compuesta por el objeto Pelo()
