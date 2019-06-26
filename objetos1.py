4c56daa12a50cd24bf28ee39b90d784fd2cf8799
terricolatomico <terricolatomico@gmail.com>
2019-06-26 12:22:20 +0100

    objetos y atributos

--- objetos1.py
+++ objetos1.py
@@ -7,9 +7,37 @@ class Pelo():
 class Ojo():
     color = ""
     longitud = ""
-class objeto():
-    color = ""
-    longitud = ""
+
+class Objeto(object):
+    color = "verde"
+    longitud = "mediana"
+    tamanio = "grande"
+    aspecto = "feo"
     antenas = Antena()  #propiedad compuesta por el objeto Antena()
     ojos = Ojo()        #propiedad compuesta por el objeto Ojo()
     pelos = Pelo()      #propiedad compuesta por el objeto Pelo()
+
+
+    def flotar(self):
+        return("flotar")
+
+
+class Dedo(object):
+    longitud = ""
+    forma = ""
+    color = ""
+
+class Pie(object):
+    forma = ""
+    color = ""
+    dedos = Dedo()
+
+    def amputar(self):
+        return ("se corto el pie")
+
+
+class NuevoObjeto(object):
+    pie = Pie()
+
+    def saltar(self):
+        return("saltar")
---
+++ principal.py
@@ -0,0 +1,22 @@
+from objetos1 import *
+
+# et = Objeto()
+# print(et.color)
+# print(et.tamanio)
+# print(et.aspecto)
+# et.color = "rosa"
+# print(et.color)
+
+objeto = Objeto( )
+print(Objeto.color)
+Objeto.tamanio = "pequenio"
+
+x = NuevoObjeto()
+
+print(x.pie.color)
+
+v = x.pie.amputar()
+print(v)
+
+
+
